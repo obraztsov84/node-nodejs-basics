4a474d013631036e63c2d431560891f970e22a40
sergei.obraztsov <sergei.obraztsov@sshvps.net>
2022-12-05 19:18:36 +0300

    feat: implement create function

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,20 @@
+import * as fs from 'fs';
+
+
 const create = async () => {
-    // Write your code here 
+    if (fs.existsSync('src/fs/files/fresh.txt')) {
+        console.error('FS operation failed')
+        return
+    }
+    fs.open('src/fs/files/fresh.txt', 'w', (err) => {
+        if (err) console.error(err)
+    })
+    fs.appendFile('src/fs/files/fresh.txt', 'I am fresh and young',(err) => {
+        if (err) console.error(err)
+
+    })
+
+
 };
 
 await create();
